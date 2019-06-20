da0345d456572e6a0ac59e9edf4eb18166eee44e
Yuanok2 <382547456@qq.com>
2019-06-20 14:21:08 +0800

    基础布局

---
+++ bootstrap/helpers.php
@@ -0,0 +1,8 @@
+<?php
+    function test(){
+    return 'ok';
+}
+//此方法会将当前请求的路由名称转换为 CSS 类名称
+    function route_class(){
+        return str_replace('.','-',Route::currentRouteName());
+    }
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+<div class="container">
+<a href="#" class="navbar-brand">
+    Laravel-Shop
+</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggle-icon"></span>
+</button>
+
+    <div id="navbarSupportContented" class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+
+    </ul>
+
+    <ul class="navbar-nav navbar-right">
+        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">注册</a></li>
+    </ul>
+    </div>
+</div>
+</nav>
+
